a8e24c3c3d32873e8b86e2a81ef9d80a4f09b939
ghostik-gh <feodor200@mail.ru>
2023-06-15 01:37:58 +0300

    minIO server + upload file

--- manager/file_uploader.py
+++ manager/file_uploader.py
@@ -6,26 +6,26 @@ def main():
     # Create a client with the MinIO server playground, its access key
     # and secret key.
     client = Minio(
-        "play.min.io",
-        access_key="Q3AM3UQ867SPQQA43P2F",
-        secret_key="zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
+        "127.0.0.1:9000",
+        access_key="adminadminadmin",
+        secret_key="rootrootroot",
+        secure=False
     )
 
-    # Make 'asiatrip' bucket if not exist.
-    found = client.bucket_exists("asiatrip")
-    if not found:
-        client.make_bucket("asiatrip")
-    else:
-        print("Bucket 'asiatrip' already exists")
+
+    # Make 'my-bucket' bucket if not exist.
+    client.make_bucket("my-bucket")
+    # found = client.bucket_exists("my-bucket")
+    # if not found:
+    #     client.make_bucket("my-bucket")
+    # else:
+    #     print("Bucket 'my-bucket' already exists")
+    print("Bucket 'my-bucket' created")
 
     # Upload '/home/user/Photos/asiaphotos.zip' as object name
     # 'asiaphotos-2015.zip' to bucket 'asiatrip'.
     client.fput_object(
-        "asiatrip", "asiaphotos-2015.zip", "/home/user/Photos/asiaphotos.zip",
-    )
-    print(
-        "'/home/user/Photos/asiaphotos.zip' is successfully uploaded as "
-        "object 'asiaphotos-2015.zip' to bucket 'asiatrip'."
+        bucket_name="my-bucket", object_name= "test.txt", file_path="C:\\Users\\Фёдор\\OneDrive\\Документы\\MTUCI\\back-practice\\Readme.md",
     )
 
 
@@ -34,3 +34,19 @@ try:
     main()
 except S3Error as exc:
     print("error occurred.", exc)
+
+# import boto3
+
+# # установка параметров подключения к MinIO
+# s3 = boto3.client(
+#     's3',
+#     endpoint_url='http://localhost:9000',
+#     aws_access_key_id='minio',
+#     aws_secret_access_key='minio123'
+# )
+
+# # загрузка файла в бакет
+# filename = 'file.txt'
+# bucket_name = 'my-bucket'
+# with open(filename, 'rb') as f:
+#     s3.upload_fileobj(f, bucket_name, filename)
