387d3fdf6805c85246dfc969ce4c81d47f7ab247
ghostik-gh <feodor200@mail.ru>
2023-06-15 16:18:29 +0300

    connect fastAPI and MinIO

--- manager/file_uploader.py
+++ manager/file_uploader.py
@@ -1,109 +1,44 @@
-import random
 from minio import Minio
 from minio.error import S3Error
 
+first_bucket='bucket01'
 
 class s3_minio():
 
-    # def __init__(self) -> None:
-    #     client = Minio(
-    #     "127.0.0.1:9000",
-    #     access_key="adminadminadmin",
-    #     secret_key="rootrootroot",
-    #     secure=False
-    # )
-
-    client = Minio(
+    def __init__(self) -> None:
+        self.client = Minio(
         "127.0.0.1:9000",
         access_key="adminadminadmin",
         secret_key="rootrootroot",
-        secure=False
+        secure=False,
+
     )
 
     def create_bucket(self):
-        if self.client.bucket_exists("bucket01"):
-            print("Bucket 'bucket01' already exists")
-        else:
-            self.client.make_bucket("bucket01")
-            print("Bucket 'bucket01' created")
+        try: 
+            if not self.client.bucket_exists(first_bucket):
+                self.client.make_bucket(first_bucket)
+                print(f"Bucket {first_bucket} created")
+            else:
+                print(f"Bucket {first_bucket} already exists")
+        except S3Error as exc:
+            print("error occurred.", exc)
 
-    def upload_file(self, file_id : str):
-        self.create_bucket()
-        self.client.fput_object(
-            bucket_name="bucket01", object_name=file_id, file_path="C:\\Users\\Фёдор\\OneDrive\\Документы\\MTUCI\\back-practice\\docker-compose.yml", 
-        )
+    def upload_file(self, file_id : str, file_path : str):
+        try:
+            self.create_bucket()
+            self.client.fput_object(
+                bucket_name=first_bucket, object_name=file_id, file_path=file_path,
+            )
+        except S3Error as exc:
+            print("error occurred.", exc)
+        
 
-    def get_url_to_file(self, file_id : str):
-        self.create_bucket()
-        url = self.client.presigned_get_object(
-            bucket_name="bucket01", object_name=file_id
-        )
-        print(url)
-        return url
-
-
-try:
-    file_id = str(random.randint(0, 1000))+'.txt'
-    s3_minio().upload_file(file_id=file_id)
-    s3_minio().get_url_to_file(file_id=file_id)
-except S3Error as exc:
-    print("error occurred.", exc)
-
-
-# def main():
-#     # Create a client with the MinIO server playground, its access key
-#     # and secret key.
-#     client = Minio(
-#         "127.0.0.1:9000",
-#         access_key="adminadminadmin",
-#         secret_key="rootrootroot",
-#         secure=False
-#     )
-
-
-#     # Make 'bucket01' bucket if not exist.
-#     # client.make_bucket("bucket01")
-#     found = client.bucket_exists("bucket01")
-#     if not found:
-#         client.make_bucket("bucket01")
-#         print("Bucket 'bucket01' created")
-#     else:
-#         print("Bucket 'bucket01' already exists")
-
-#     file_id = str(random.randint(0, 1000))+'.txt'
-
-#     client.fput_object(
-#         bucket_name="bucket01", object_name=file_id, file_path="C:\\Users\\Фёдор\\OneDrive\\Документы\\MTUCI\\back-practice\\docker-compose.yml", 
-#     )
-
-#     result = client.stat_object(
-#         bucket_name="bucket01", object_name=file_id
-#     )
-
-#     print(
-#     "last-modified: {0}, size: {1}".format(
-#         result.last_modified, result.size,
-#     ),)
-
-#     url = client.presigned_get_object(
-#         bucket_name="bucket01", object_name=file_id
-#     )
-
-#     print(url)
-
-
-# import boto3
-
-# # установка параметров подключения к MinIO
-# s3 = boto3.client(
-#     's3',
-#     endpoint_url='http://localhost:9000',
-#     aws_access_key_id='minio',
-#     aws_secret_access_key='minio123'
-# )
-
-# # загрузка файла в бакет
-# filename = 'file.txt'
-# bucket_name = 'bucket01'
-# with open(filename, 'rb') as f:
-#     s3.upload_fileobj(f, bucket_name, filename)
+    def get_url_to_file(self, file_id : str) -> str:
+        try: 
+            url = self.client.presigned_get_object(
+                bucket_name=first_bucket, object_name=file_id
+            )
+            return url 
+        except S3Error as exc:
+            print("error occurred.", exc)
--- manager/main.py
+++ manager/main.py
@@ -1,5 +1,8 @@
-import random
 from fastapi import FastAPI, UploadFile
+import os
+import file_uploader
+import uuid
+
 
 app = FastAPI()
 
@@ -7,14 +10,12 @@ app = FastAPI()
 # TODO FastAPI-users[sqlalchemy] для авторизации
 
 """
-Проверка состояния 
-по айди файла ?
+Проверка состояния по uuid файла
 """
-@app.get("/check/{filename}")
-def check_state(filename: str):
-    if 1 == random.randint(0, 1):
-        return {'status': "Пока не готово", 'ID': filename}
-    return {"Вы можете получить свои данные https://"}
+@app.get("/check/{file_id}")
+def check_state(file_id: str):
+    return {'status': 200, 'response': f"Вы можете получить свои данные http://127.0.0.1:8000/download/{file_id}"}
+    return {'status': 425, 'response': "Идет обработка"}
 
 """
 Отправка файла в MinIO
@@ -22,20 +23,36 @@ def check_state(filename: str):
 """
 @app.post("/upload/file")
 def upload_file(file: UploadFile):
-    with open(f'../data/{file.filename}', "wb+") as f:
+    rel_dir_path='../tmp'
+    file_id = str(uuid.uuid4())
+
+    if not os.path.exists(rel_dir_path):
+        os.makedirs(rel_dir_path)
+    
+    with open(f'{rel_dir_path}/{file.filename}', "wb+") as f:
         f.write(file.file.read())
-    return file
+
+    abs_path = os.path.abspath(f'{rel_dir_path}/{file.filename}')
+
+    try: 
+        minio_cl = file_uploader.s3_minio()
+        minio_cl.upload_file(file_id=file_id, file_path=abs_path)
+        return {'status': 200, 'id' : file_id}
+    except Exception as err:
+        return{'status': 500, 'error': err}
+    finally:
+        os.remove(path=abs_path)
 
 """
 Скачивание файла 
 через S3 minio ? или раздача через апи 
 просто передавать ссылку ? 
 """
-@app.get("/download/file")
-def download_file(file: UploadFile):
-    with open(f'../data/{file.filename}', "wb+") as f:
-        f.write(file.file.read())
-    return file
+@app.get("/download/{file_id}")
+def download_file(file_id: str):
+    minio_cl = file_uploader.s3_minio()
+    url = minio_cl.get_url_to_file(file_id=file_id)
+    return {'status' : 200,"url": url}
 
 """
 uvicorn main:app --reload
