74ab6a639de1ffb76d5fa80a4dbe68892297f6c2
ghostik-gh <feodor200@mail.ru>
2023-06-15 16:21:04 +0300

    rename

--- manager/main.py
+++ manager/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, UploadFile
 import os
-import file_uploader
+from s3_minio import s3_minio
 import uuid
 
 
@@ -35,7 +35,7 @@ def upload_file(file: UploadFile):
     abs_path = os.path.abspath(f'{rel_dir_path}/{file.filename}')
 
     try: 
-        minio_cl = file_uploader.s3_minio()
+        minio_cl = s3_minio()
         minio_cl.upload_file(file_id=file_id, file_path=abs_path)
         return {'status': 200, 'id' : file_id}
     except Exception as err:
@@ -50,7 +50,7 @@ def upload_file(file: UploadFile):
 """
 @app.get("/download/{file_id}")
 def download_file(file_id: str):
-    minio_cl = file_uploader.s3_minio()
+    minio_cl = s3_minio()
     url = minio_cl.get_url_to_file(file_id=file_id)
     return {'status' : 200,"url": url}
 
