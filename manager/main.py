4876924225cb083dce1e40308fc3428a3b9d5987
ghostik-gh <feodor200@mail.ru>
2023-06-17 01:20:27 +0300

    from minio to boto3

--- manager/main.py
+++ manager/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI, UploadFile
-import os
+import uvicorn
 from s3_minio import s3_minio
 import uuid
 import postgre 
 
 app = FastAPI()
 
-
 # TODO FastAPI-users[sqlalchemy] для авторизации
+
+# TODO Pydantic (BaseModel)
+# YEP: boto3 вместо minio 
+# TODO raise http error
+# TODO secret keys
+# 
 
 """
 Проверка состояния по uuid файла
@@ -17,15 +22,15 @@ def check_state(file_id: str):
     status = postgre.get_status(file_id)
     match status:
         case 0:
-            return {'status': 425, 'response': "Файл успешно загружен"}
+            return {'status': 200, 'response': "Файл успешно загружен"}
         case 1:
-            return {'status': 425, 'response': "Вы находитесь в очереди"}
+            return {'status': 200, 'response': "Вы находитесь в очереди"}
         case 2:
-            return {'status': 425, 'response': "Идет обработка"}
+            return {'status': 200, 'response': "Идет обработка"}
         case 3:
             return {'status': 200, 'response': f"Вы можете получить свои данные http://127.0.0.1:8000/download/{file_id}"}
         case 4:
-            return {'status': 404, 'response': "Файл уже удален"}
+            return {'status': 200, 'response': "Файл уже удален"}
         case _:
             return {'status': 500, 'response': "Неверное состояние файла"}
 
@@ -33,28 +38,18 @@ def check_state(file_id: str):
 Отправка файла в MinIO
 добавление состояния в таблицу
 """
-@app.post("/upload/file")
+@app.post("/upload/")
 def upload_file(file: UploadFile):
-    rel_dir_path='../tmp'
     file_id = str(uuid.uuid4())
 
-    if not os.path.exists(rel_dir_path):
-        os.makedirs(rel_dir_path)
-    
-    with open(f'{rel_dir_path}/{file.filename}', "wb+") as f:
-        f.write(file.file.read())
-
-    abs_path = os.path.abspath(f'{rel_dir_path}/{file.filename}')
-
     try: 
-        minio_cl = s3_minio()
-        minio_cl.upload_file(file_id=file_id, file_path=abs_path)
-        postgre.add_file(file_id, file.filename, file.filename, '.abc')
-        return {'status': 200, 'id' : file_id}
+        s3 = s3_minio()
+        s3.upload_file(file=file.file, file_id=file_id)
+        print("upload")
+        # postgre.add_file(file_id, file.filename, file.filename, '.abc')
+        return {'status': 200, 'id' : file_id, 'response': f"http://127.0.0.1:8000/download/{file_id}"}
     except Exception as err:
         return{'status': 500, 'error': err}
-    finally:
-        os.remove(path=abs_path)
 
 """
 Скачивание файла 
@@ -65,7 +60,7 @@ def download_file(file_id: str):
     # if status == 3:
     minio_cl = s3_minio()
     url = minio_cl.get_url_to_file(file_id=file_id)
-    remove_file(file_id=file_id) # удаление файла ?
+    # remove_file(file_id=file_id) # удаление файла ?
     return {'status' : 200, "url": url}
 
 """
@@ -81,8 +76,8 @@ def remove_file(file_id: str):
         return{'status': 500, 'error': err}
 
 """
+cd manager
 uvicorn main:app --reload
-useless ?
 """
 if __name__=="__main__":
-    pass
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level='debug')
--- manager/s3_minio.py
+++ manager/s3_minio.py
@@ -1,49 +1,65 @@
-from minio import Minio
-from minio.error import S3Error
+from config import settings
+import boto3
+
 
 first_bucket='bucket01'
 
 class s3_minio():
 
     def __init__(self) -> None:
-        self.client = Minio(
-        "127.0.0.1:9000",
-        access_key="adminadminadmin",
-        secret_key="rootrootroot",
-        secure=False,
-    )
+
+        self.client = boto3.client('s3',
+                    endpoint_url=f'http://{settings.HOST}:{settings.PORT_MINIO}',
+                    aws_access_key_id=settings.MINIO_ROOT_USER,
+                    aws_secret_access_key=settings.MINIO_ROOT_PASSWORD,
+                    # config=Config(signature_version='s3v4'),
+                    # region_name='us-east-1'
+                    )
+    s3_resource = boto3.resource('s3',
+                endpoint_url=f'http://{settings.HOST}:{settings.PORT_MINIO}',
+                aws_access_key_id=settings.MINIO_ROOT_USER,
+                aws_secret_access_key=settings.MINIO_ROOT_PASSWORD)
+
 
     def create_bucket(self):
+        
+        # try:
+        #     s3.head_bucket(Bucket=bucket_name)
+        #     print(f"The bucket {bucket_name} exists")
+        # except ClientError:
+        #     print(f"The bucket {bucket_name} does not exist")
         try: 
-            if not self.client.bucket_exists(first_bucket):
-                self.client.make_bucket(first_bucket)
-                print(f"Bucket {first_bucket} created")
-            else:
-                print(f"Bucket {first_bucket} already exists")
-        except S3Error as exc:
-            print("error occurred.", exc)
+            if not first_bucket in self.client.buckets.all():
+                bucket = self.client.create_bucket(Bucket=first_bucket)
+        except Exception as exc:
+            print("[ERROR]:", exc)
+        finally:
+            return bucket
 
-    def upload_file(self, file_id : str, file_path : str):
+    def upload_file(self, file, file_id : str):
         try:
-            self.create_bucket()
-            self.client.fput_object(
-                bucket_name=first_bucket, object_name=file_id, file_path=file_path,
-            )
-        except S3Error as exc:
-            print("error occurred.", exc)
+            self.client.upload_fileobj(file, first_bucket, file_id)
+        except Exception as exc:
+            print("[ERROR]:", exc)
         
 
     def get_url_to_file(self, file_id : str) -> str:
         try: 
-            url = self.client.presigned_get_object(
-                bucket_name=first_bucket, object_name=file_id
-            )
+            url = self.client.generate_presigned_url(
+                'get_object',
+                Params={
+                    'Bucket': first_bucket,
+                    'Key': file_id
+                },
+                )
             return url 
-        except S3Error as exc:
-            print("error occurred.", exc)
+        except Exception as exc:
+            print("[ERROR]:", exc)
+
 
     def remove_file(self, file_id: str):
         try: 
-            self.client.remove_object(bucket_name=first_bucket, object_name=file_id,)
-        except S3Error as exc:
-            print("error occurred.", exc)
+            self.client.delete_object(Bucket=first_bucket, Key=file_id,)
+        except Exception as exc:
+            print("[ERROR]:", exc)
+
