6c4a58af475dc6344b2484602f6fd69604f7b846
ghostik-gh <feodor200@mail.ru>
2023-06-18 02:04:29 +0300

    ffmpeg in progress

--- converter/rabbit.py
+++ converter/rabbit.py
@@ -2,6 +2,8 @@ import pika
 from config import settings
 from postgre import change_status
 import time
+from ffmpeg import FFmpeg, Progress
+from s3_minio import S3minio
 
 
 
@@ -23,7 +25,35 @@ def callback(ch, method, properties, body):
     print(body)
     print("Received %r" % body)
     change_status(body, 2)
-    time.sleep(2.5)
+
+    file = S3minio().get_file(body).get('Body')    
+    print(file)
+    print("========================================")
+
+    # def main():
+    ffmpeg = (
+        FFmpeg()
+        .option("y")
+        .input("long.mp4")
+        .output(
+            "out",
+            {"codec:a": "pcm_s16le"},
+            vn=None,
+            f="wav",
+        )
+    )
+
+    @ffmpeg.on("progress")
+    def on_progress(progress: Progress):
+        print(progress)
+
+    print("========================================")
+    ffmpeg.execute()
+    # wave_bytes = ffmpeg.execute()
+    # print(wave_bytes)
+
+
+    time.sleep(10)
     change_status(body, 3)
 
 
@@ -31,3 +61,4 @@ def callback(ch, method, properties, body):
 channel.basic_consume(queue='hello',
                       on_message_callback=callback,
                       auto_ack=True)
+
---
+++ converter/s3_minio.py
@@ -0,0 +1,69 @@
+from config import settings
+import boto3
+import botocore
+
+first_bucket='bucket01'
+upload_bucket='upbuck'
+download_bucket='downbuck'
+
+class S3minio():
+
+    def __init__(self) -> None:
+
+        self.s3_resource = boto3.resource('s3',
+                endpoint_url=f'http://{settings.HOST}:{settings.PORT_MINIO}',
+                aws_access_key_id=settings.MINIO_ROOT_USER,
+                aws_secret_access_key=settings.MINIO_ROOT_PASSWORD)
+
+        self.client = self.s3_resource.meta.client
+
+
+    def create_bucket(self, bucket):
+        exists = True
+        try:
+            self.s3_resource.meta.client.head_bucket(Bucket=bucket)
+        except botocore.exceptions.ClientError as e:
+            error_code = e.response['Error']['Code']
+            if error_code == '404':
+                exists = False
+
+        if not exists:
+            try:
+                self.s3_resource.create_bucket(Bucket=bucket)
+            except Exception as err:
+                print(err)
+
+
+    def upload_file(self, file, file_id : str):
+        self.create_bucket(download_bucket)
+        try:
+            self.client.upload_fileobj(file, download_bucket, file_id)
+        except Exception as exc:
+            print("[ERROR]:", exc)
+        
+    
+    def get_file(self, file_id : str):
+        file = self.client.get_object(Bucket=upload_bucket, Key=file_id)
+        return file
+
+
+    def get_url_to_file(self, file_id : str) -> str:
+        try: 
+            url = self.client.generate_presigned_url(
+                'get_object',
+                Params={
+                    'Bucket': first_bucket,
+                    'Key': file_id
+                },
+                )
+            return url 
+        except Exception as exc:
+            print("[ERROR]:", exc)
+
+
+    def remove_file(self, file_id: str, bucket:str):
+        try: 
+            self.client.delete_object(Bucket=bucket, Key=file_id,)
+        except Exception as exc:
+            print("[ERROR]:", exc)
+
