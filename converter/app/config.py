37084a68f4e123b31367a4e1999094b7f20e4040
ghostik-gh <feodor200@mail.ru>
2023-06-23 18:57:22 +0300

    fix env

--- converter/app/config.py
+++ converter/app/config.py
@@ -7,17 +7,17 @@ import os
 load_dotenv(find_dotenv())
 
 class Settings(BaseSettings):
-
-    DB_USER = os.getenv('DB_USER')
-    DB_PASSWORD = os.getenv('DB_PASSWORD')
-    MINIO_ROOT_USER = os.getenv('MINIO_ROOT_USER')
-    MINIO_ROOT_PASSWORD = os.getenv('MINIO_ROOT_PASSWORD')
+    MINIO_ROOT_USER=os.getenv('MINIO_ROOT_USER')
+    MINIO_ROOT_PASSWORD=os.getenv('MINIO_ROOT_PASSWORD')
     RABBITMQ_DEFAULT_USER=os.getenv('RABBITMQ_DEFAULT_USER')
     RABBITMQ_DEFAULT_PASS=os.getenv('RABBITMQ_DEFAULT_PASS')
-    HOST = os.getenv('HOST')
-    PORT_DB = os.getenv('PORT_DB')
-    PORT_MINIO = os.getenv('PORT_MINIO')
-    PORT_APP = os.getenv('PORT_APP')
+    DSN_POSTGRE=os.getenv('DSN_POSTGRE')
+    ENDPOINT_MINIO=os.getenv('ENDPOINT_MINIO')
+    HOST_RABBITMQ=os.getenv('HOST_RABBITMQ')
+    HOST=os.getenv('HOST')
+    PORT_DB=os.getenv('PORT_DB')
+    PORT_MINIO=os.getenv('PORT_MINIO')
+    PORT_APP=os.getenv('PORT_APP')
     PORT_RABBITMQ=os.getenv('PORT_RABBITMQ')
 
 
